8e428cc84241935a2a4748cca6bef549c9d994a9
Lucas <lucasedu.comercial@gmail.com>
2024-12-16 08:20:53 -0300

    auth

---
+++ config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbHOwmCPDJkoopcN_vHVRLYc7kejmYgTc",
+  authDomain: "anotations-4c948.firebaseapp.com",
+  projectId: "anotations-4c948",
+  storageBucket: "anotations-4c948.firebasestorage.app",
+  messagingSenderId: "940884295975",
+  appId: "1:940884295975:web:c4cdda7c8cd8ecf868e902",
+  measurementId: "G-52EK5Q3PFF",
+};
+
+const senhaCorreta = "Senha123@";
+
+window.firebaseConfig = firebaseConfig;
+window.senhaCorreta = senhaCorreta;
